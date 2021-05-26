d4339aafbc357d65031bda6f5de26bfaba44a349
unknown <zohaibuii@gmail.com>
2021-05-26 10:40:17 +0500

    first commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,18 +68,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to My Introduction</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<video width="320" height="240" controls>
+		  <source src="<?php echo base_url() ?>/asset/intro_info/mov_bbb.mp4" type="video/mp4">
+		  <source src="<?php echo base_url() ?>/asset/intro_info/mov_bbb.ogg" type="video/ogg">
+		Your browser does not support the video tag.
+		</video>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
